b5f57c67dd4600f52cc121121ca1f39da4bea8c5
pkillthetoy <tonyzhao099@gmail.com>
2017-03-19 12:32:30 -0700

    First Babel commit
    Add a gitignore rule, and a very simple hello world babel app.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pathofeternity.github.io
 Path of Eternity Game
+
+Run npm install from within the pathofeternity directory (should see a package.json file).
---
+++ pathofeternity/js/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+ReactDOM.render(React.createElement(
+  'h1',
+  null,
+  'Hello, ssss!'
+), document.getElementById('root'));
+//# sourceMappingURL=E:\Desktop\pathofeternity\pathofeternity.github.io\pathofeternity\index.js.map
