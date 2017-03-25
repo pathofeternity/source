8bed5e57d95b1937a230ef9817e40b31a11fa4f2
pkillthetoy <tonyzhao099@gmail.com>
2017-03-25 12:16:43 -0700

    Break small bars into smaller component. Clean up breakthrough button.

---
+++ pathofeternity/src/small_bars.js
@@ -0,0 +1,51 @@
+import React, { PropTypes } from 'react';
+import './training_bars.css';
+import { connect } from 'react-redux'
+import {ProgressBar, Button, Fade} from 'react-bootstrap'
+
+const SmallBarLayout = ({name, value, max, rate}) =>  (
+    <div className="small bars-container">
+      <h3>{name}</h3>
+      <ProgressBar max={max} now={value}
+        label={<span>{value} / {max} ({rate}/sec)</span>} />
+    </div>
+  )
+  SmallBarLayout.propTypes = {
+    name: PropTypes.string.isRequired,
+    value: PropTypes.number.isRequired,
+    max: PropTypes.number.isRequired,
+    rate: PropTypes.number.isRequired
+  }
+
+const mapDispatchToProps = (dispatch) => {return {}}
+const statRate = (stat) => stat.percent * stat.rate / 100
+const mapStateToBodyProps = (state) => {
+  return {
+    name: "Body",
+    value: Number(state.scores.body.toFixed(2)),
+    max: state.stats.body.max,
+    rate: statRate(state.stats.body),
+  }
+}
+const mapStateToMindProps = (state) => {
+  return {
+    name: "Mind",
+    value: Number(state.scores.mind.toFixed(2)),
+    max: state.stats.mind.max,
+    rate: statRate(state.stats.mind),
+  }
+}
+const mapStateToSoulProps = (state) => {
+  return {
+    name: "Soul",
+    value: Number(state.scores.soul.toFixed(2)),
+    max: state.stats.soul.max,
+    rate: statRate(state.stats.soul),
+  }
+}
+
+const BodyBar = connect(mapStateToBodyProps, mapDispatchToProps)(SmallBarLayout)
+const MindBar = connect(mapStateToMindProps, mapDispatchToProps)(SmallBarLayout)
+const SoulBar = connect(mapStateToSoulProps, mapDispatchToProps)(SmallBarLayout)
+
+export {BodyBar, MindBar, SoulBar}
--- pathofeternity/src/training_bars.js
+++ pathofeternity/src/training_bars.js
@@ -1,64 +1,43 @@
 import React, { PropTypes } from 'react';
 import './training_bars.css';
 import { connect } from 'react-redux'
-import {ProgressBar, Button} from 'react-bootstrap'
+import {ProgressBar, Button, Fade} from 'react-bootstrap'
+import {BodyBar, MindBar, SoulBar} from './small_bars.js'
 
-const spanOrButton = (value, max) => {
-  if (value === max) {
-    return <Button>Breakthrough</Button>
+const spanOrButton = (value, max, rate) => {
+  if (value >= max) {
+    return <span className="nomargin"><Button>Breakthrough</Button></span>
   } else {
-    return <span>{value} / {max}</span>
+    return <span>{value} / {max} ({rate}/sec)</span>
   }
 }
 
-const BarsLayout = ({cultivation, body, mind, soul,
-cultivationMax, bodyMax, mindMax, soulMax}) =>  (
+const BarsLayout = ({cultivation, cultivationMax, cultivationRate}) =>  (
   <div className="bars-component">
     <div className="bars-container">
       <h2>Cultivation</h2>
       <ProgressBar max={cultivationMax} now={cultivation}
-        label={spanOrButton(cultivation, cultivationMax)} />
+        label={spanOrButton(cultivation, cultivationMax, cultivationRate)} />
     </div>
 
-    <div className="small bars-container">
-      <h3>Body</h3>
-      <ProgressBar max={bodyMax} now={body}
-        label={<span>{body} / {bodyMax}</span>} />
-    </div>
-    <div className="small bars-container">
-      <h3>Mind</h3>
-      <ProgressBar max={mindMax} now={mind}
-        label={<span>{mind} / {mindMax}</span>} />
-    </div>
-    <div className="small bars-container">
-      <h3>Soul</h3>
-      <ProgressBar max={soulMax} now={soul}
-        label={<span>{soul} / {soulMax}</span>} />
-    </div>
+    <BodyBar/>
+    <MindBar/>
+    <SoulBar/>
   </div>
   )
   BarsLayout.propTypes = {
     cultivation: PropTypes.number.isRequired,
     cultivationMax: PropTypes.number.isRequired,
-    body: PropTypes.number.isRequired,
-    bodyMax: PropTypes.number.isRequired,
-    mind: PropTypes.number.isRequired,
-    mindMax: PropTypes.number.isRequired,
-    soul: PropTypes.number.isRequired,
-    soulMax: PropTypes.number.isRequired,
+    cultivationRate: PropTypes.number.isRequired,
   }
 
 const mapDispatchToProps = (dispatch) => {return {}}
+const statRate = (stat) => stat.percent * stat.rate / 100
 const mapStateToProps = (state) => {
   return {
-    cultivation: state.scores.cultivation,
+    cultivation: Number(state.scores.cultivation.toFixed(2)),
     cultivationMax: state.stats.cultivation.max,
-    body: state.scores.body,
-    bodyMax: state.stats.body.max,
-    mind: state.scores.mind,
-    mindMax: state.stats.mind.max,
-    soul: state.scores.soul,
-    soulMax: state.stats.soul.max,
+    cultivationRate: statRate(state.stats.cultivation),
   }
 }
 
