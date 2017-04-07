f1e9618ac9dcc8989f9891bf126a700f928f6821
pkillthetoy <tonyzhao099@gmail.com>
2017-04-06 19:05:56 -0700

    Items area placeholders

---
+++ pathofeternity/src/tab_menu/items_menu.js
@@ -0,0 +1,37 @@
+import React, { PropTypes } from 'react';
+import './items_menu.css'
+import { connect } from 'react-redux'
+import {Button, Accordion} from 'react-bootstrap'
+import {setPercent} from '../actions.js'
+
+// Actually the right side of the training menu, includes foundation slider.
+const ItemsMenuLayout = ({cultivation, body, mind, soul, onChange}) => (
+  <div className="items-column-container">
+    <div className="thin"><h3>Foundation and bar</h3></div>
+    <div className="thin">
+      Placeholder:
+      <div className="small-slider">
+        <input type="range" min="0" max="100"></input>
+      </div>
+      25%
+    </div>
+    <div className="thick">
+      Herb <Button>Use</Button><input type="number" defaultValue="1" min="1"></input>
+    </div>
+  </div>
+)
+
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+  }
+}
+const mapStateToProps = (state) => {
+  return {
+  }
+}
+
+
+const ItemsMenu = connect(mapStateToProps, mapDispatchToProps)(ItemsMenuLayout)
+
+export default ItemsMenu
--- pathofeternity/src/tab_menu/tab_menu.js
+++ pathofeternity/src/tab_menu/tab_menu.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux'
 import {Tabs, Tab} from 'react-bootstrap'
 import TrainingMenu from './training_menu.js'
+import ItemsMenu from './items_menu.js'
 import './tab_menu.css'
 
 const TabLayout = () => (
@@ -9,6 +10,7 @@ const TabLayout = () => (
       <Tab eventKey={1} title="Training">
         <div className="tab-container">
           <TrainingMenu />
+          <ItemsMenu />
         </div>
       </Tab>
       <Tab eventKey={2} title="Sects">Tab 2 content</Tab>
