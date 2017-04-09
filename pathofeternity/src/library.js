aac25b5c0b05adc3760c0f24b348ef4e399501bb
pkillthetoy <tonyzhao099@gmail.com>
2017-04-09 11:03:37 -0700

    Reorder statements to get around warning.

--- pathofeternity/src/library.js
+++ pathofeternity/src/library.js
@@ -1,8 +1,8 @@
+const tierNames = ["Essence", "Foundation", "Azoth", "Nascent Soul", "Demigod"];
+
 export function levelName (cultivation) {
   var levelNumber = Math.floor(Math.log10(cultivation - 1));
   var tier = Math.floor(levelNumber / 10);
   var level = levelNumber % 10;
   return tierNames[tier] + " " + level;
 }
-
-const tierNames = ["Essence", "Foundation", "Azoth", "Nascent Soul", "Demigod"];
