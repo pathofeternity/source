35396b3672c934dd282420b7dc810b8d40c726ce
pkillthetoy <tonyzhao099@gmail.com>
2017-04-22 12:49:59 -0700

    Display skill training sliders.
    Skill sliders automatically disappear when a skill's XP is removed from
    scores, which is something that will be implemented when
    
    Slider setting action still needs to be changed. Tick also needs to
    handle level ups.

--- pathofeternity/src/tab_menu/training_menu.js
+++ pathofeternity/src/tab_menu/training_menu.js
@@ -1,11 +1,12 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import './training_menu.css'
+import {SKILLS} from '../skills.js'
 import { connect } from 'react-redux'
 import {Panel, Accordion, Fade} from 'react-bootstrap'
 import {setPercent} from '../actions.js'
 
 // Component with local state to expand/collapse panels.
-const TrainingMenuLayout = ({cultivation, body, mind, soul, cultivationMax, onChange}) => (
+const TrainingMenuLayout = ({cultivation, body, mind, soul, cultivationMax, onChange, availableSkills, scores}) => (
 	<div className="training-container">
 
 		<Accordion>
@@ -13,48 +14,55 @@ const TrainingMenuLayout = ({cultivation, body, mind, soul, cultivationMax, onCh
 				<table><tbody><tr>
 					<td>Meditation</td>
 					<td><input type="range" min="0" max="100"
-					value={cultivation}
-					onChange={e => {onChange("cultivation", e)}}/></td>
+						value={cultivation}
+						onChange={e => {onChange("cultivation", e)}}/></td>
 					<td>{cultivation}%</td>
 				</tr></tbody></table>
 			</Panel>
 			<Fade in={cultivationMax >= 1e2}>
 				<Panel header={"Basic (" + (body + mind + soul) + "%)"}>
 					<table><tbody>
-					<tr>
-						<td>Physical Training</td>
-						<td><input type="range" min="0" max="100"	value={body} onChange={e => {onChange("body", e)}}/></td>
-						<td>{body}%</td>
-					</tr>
-					<Fade in={cultivationMax >= 1e6}>
 						<tr>
-							<td>Mental Training</td>
-							<td><input type="range" min="0" max="100"	value={mind} onChange={e => {onChange("mind", e)}}/></td>
-							<td>{mind}%</td>
+							<td>Physical Training</td>
+							<td><input type="range" min="0" max="100"	value={body} onChange={e => {onChange("body", e)}}/></td>
+							<td>{body}%</td>
 						</tr>
-					</Fade>
-					<Fade in={cultivationMax >= 1e26}>
-						<tr>
-							<td>Soul Training</td>
-							<td><input type="range" min="0" max="100"	value={soul} onChange={e => {onChange("soul", e)}}/></td>
-							<td>{soul}%</td>
-						</tr>
-					</Fade>
+						<Fade in={cultivationMax >= 1e6}>
+							<tr>
+								<td>Mental Training</td>
+								<td><input type="range" min="0" max="100"	value={mind} onChange={e => {onChange("mind", e)}}/></td>
+								<td>{mind}%</td>
+							</tr>
+						</Fade>
+						<Fade in={cultivationMax >= 1e26}>
+							<tr>
+								<td>Soul Training</td>
+								<td><input type="range" min="0" max="100"	value={soul} onChange={e => {onChange("soul", e)}}/></td>
+								<td>{soul}%</td>
+							</tr>
+						</Fade>
+					</tbody></table>
+				</Panel>
+			</Fade>
+			<Fade in={availableSkills.cultivationProficiency !== undefined}>
+				<Panel header="Skills">
+					<table><tbody>
+						{Object.keys(scores)
+							.filter(name => availableSkills[name])
+							.map((name, index) =>{
+								return <tr key={index}>
+									<td>Train {SKILLS[name].name}</td>
+									<td><input type="range" min="0" max="100"
+										value={availableSkills[name].percent}
+										onChange={e => {onChange(name, e)}}/></td>
+									<td>{availableSkills[name].percent}%</td>
+								</tr>})}
 					</tbody></table>
 				</Panel>
 			</Fade>
 		</Accordion>
 	</div>
 )
-TrainingMenuLayout.propTypes = {
-	cultivation: PropTypes.number,
-	cultivationMax: PropTypes.number,
-	body: PropTypes.number,
-	mind: PropTypes.number,
-	soul: PropTypes.number
-
-}
-
 
 const mapDispatchToProps = (dispatch) => {
 	return {
@@ -68,6 +76,8 @@ const mapStateToProps = (state) => {
 		mind: state.stats.mind.percent,
 		soul: state.stats.soul.percent,
 		cultivationMax: state.stats.cultivation.max,
+		availableSkills: state.availableSkills,
+		scores: state.scores,
 	}
 }
 
