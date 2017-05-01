4cfbeb08a3062d311e84f8f74a3247fd3704048b
pkillthetoy <tonyzhao099@gmail.com>
2017-04-30 20:47:26 -0700

    Refactor skill tooltip into a component.

--- pathofeternity/src/skill_panel/skill_panel.js
+++ pathofeternity/src/skill_panel/skill_panel.js
@@ -14,34 +14,19 @@ const equipDragStart = (event) => {
   event.dataTransfer.setData(EVENT_TYPE, EQUIP)
 }
 
-const MakeTooltip = (scores, skillName, xpRequired, totalRate, level) => {
-  if (level === SKILLS[skillName].maxLevel) {
-    return <Tooltip id="skill-tip">MAX</Tooltip>
-  }
-  return <Tooltip id="skill-tip">{scores[skillName]} / {xpRequired}, {totalRate}/sec</Tooltip>
-}
-
-const SingleSkillLayout = ({availableSkills, scores, skillName, onClick, multiplier}) => {
+const TooltipContentsLayout = ({availableSkills, scores, skillName, multiplier}) => {
+var skillObject = SKILLS[skillName]
   var level = availableSkills[skillName].level
-  var maxLevel = SKILLS[skillName].maxLevel
-  var skillObject = SKILLS[skillName]
-  var currXp = scores[skillName]
+  var xpRequired = skillObject.xpRequiredFunction(level)
   var multiplierNumber = multiplier[skillName] ? multiplier[skillName] : 1
   var totalRate = availableSkills[skillName].rate * availableSkills[skillName].percent * multiplierNumber / 100
-  var xpRequired = skillObject.xpRequiredFunction(level)
-
-  return <div onClick={onClick(skillName)}>
-    <OverlayTrigger placement="top" overlay={MakeTooltip(scores, skillName, xpRequired, totalRate, level)}>
-      <div className="name-div">
-        <img className="skill-icon" alt={skillObject.name}
-          src={skillObject.icon} draggable="false"
-        />
-        {skillObject.name} Lv. {level === maxLevel ? "MAX" : level}
-      </div>
-    </OverlayTrigger>
-      {level === maxLevel ? null : <ProgressBar className="smallProgress" max={xpRequired} now={currXp}/>}
-
-    </div>
+  return <div>
+    {level === SKILLS[skillName].maxLevel ? <div>MAX</div> :
+      <div>{scores[skillName]} / {xpRequired}, {totalRate}/sec</div>
+    }
+    <div>{SKILLS[skillName].description}</div>
+    <div>Current / Next</div>
+  </div>
 }
 
 const singleSkillDispatchToProps = (dispatch) => {
@@ -56,6 +41,29 @@ const singleSkillStateToProps = (state) => {
     scores: state.scores,
     multiplier: getTotalMultiplier(state),
   }
+}
+
+const TooltipContents = connect(singleSkillStateToProps, singleSkillDispatchToProps)(TooltipContentsLayout)
+
+const SingleSkillLayout = ({availableSkills, scores, skillName, onClick, multiplier}) => {
+  var level = availableSkills[skillName].level
+  var maxLevel = SKILLS[skillName].maxLevel
+  var skillObject = SKILLS[skillName]
+  var currXp = scores[skillName]
+  var xpRequired = skillObject.xpRequiredFunction(level)
+
+  return <div onClick={onClick(skillName)}>
+    <OverlayTrigger placement="bottom" overlay={<Tooltip id="skill-tip"><TooltipContents skillName={skillName}/></Tooltip>}>
+      <div className="name-div">
+        <img className="skill-icon" alt={skillObject.name}
+          src={skillObject.icon} draggable="false"
+        />
+        {skillObject.name} Lv. {level === maxLevel ? "MAX" : level}
+      </div>
+    </OverlayTrigger>
+    {level === maxLevel ? null : <ProgressBar className="smallProgress" max={xpRequired} now={currXp}/>}
+
+  </div>
 }
 
 const SingleSkill = connect(singleSkillStateToProps, singleSkillDispatchToProps)(SingleSkillLayout)
